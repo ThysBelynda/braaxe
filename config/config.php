4d48c6403b3f2f772f973678fa1c03390b14cf34
ThysBelynda <belynda.thys@devinci.fr>
2016-12-12 23:30:23 +0100

    test

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $host = "localhost";
 $dbName = "helloddk_test";
 $user = "root";
@@ -13,3 +13,20 @@ try {
 catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
+*/
+
+/*
+//CONFIGURE YOU APP HERE
+define("APP_ID","584723665071337");//REPLACE THIS WITH YOUR FACEBOOK APP ID
+define("APP_SECRET","148f7c2fa47fbacb8757ce2be0d1a030");//REPLACE THIS WITH YOUR FACEBOOK APP SECRET
+define("CALLBACK_URI","http://localhost/braaxe_exo/callback.php");//REPLACE THIS WITH YOU CALLBACK URL [This is where facebook will redirect user after login]
+define("LOGOUT_REDIRECT_URL","http://localhost/braaxe_exo/home.php");//REPLACE THIS WITH URL OF YOU LOGIN PAGE
+
+/** APP CONFIGURATION END HERE **/
+*/
+
+$fb = new Facebook\facebook([
+    'app_id' => '584723665071337',
+    'app_secret' => '148f7c2fa47fbacb8757ce2be0d1a030',
+    'default_graph_version' => 'v2.5',
+]);

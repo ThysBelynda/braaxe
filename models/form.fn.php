1d83f85e5e0b38a6aff36bcfc5072b8f3a21cac5
ThysBelynda <belynda.thys@devinci.fr>
2016-12-14 00:15:59 +0100

    modif php form

--- models/form.fn.php
+++ models/form.fn.php
@@ -3,7 +3,7 @@ session_start();
 require ('../config/config.php');
 
 function register(PDO $db, $nom, $prenom, $email, $postal, $telephone) {
-    $sql= "INSERT INTO users SET nom= :nom, prenom = :prenom,  email= :email, postal= :postal, telephone= :telephone , password= :password";
+    $sql="INSERT INTO users SET nom= :nom, prenom = :prenom, email= :email, postal= :postal, telephone= :telephone";
 
     $req = $db->prepare($sql);
     $req->execute(array(
